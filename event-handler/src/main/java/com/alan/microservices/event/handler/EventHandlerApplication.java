97df14d24d9068e423ea4e8211d9f6996539e1e9
alan <438162498@qq.com>
2019-12-17 16:46:23 +0800

    1217 event handler 模块整理

--- event-handler/src/main/java/com/alan/microservices/event/handler/EventHandlerApplication.java
+++ event-handler/src/main/java/com/alan/microservices/event/handler/EventHandlerApplication.java
@@ -1,36 +1,14 @@
 package com.alan.microservices.event.handler;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
 
 @SpringBootApplication
 public class EventHandlerApplication {
-    public static final Logger logger = LoggerFactory.getLogger(Producer.class);
-
-    public static void main(String[] args) throws IOException, MqttException {
-        ApplicationContext context = SpringApplication.run(EventHandlerApplication.class, args);
-
-
-
-        Config config = context.getBean(Config.class);
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(EventHandlerApplication.class, args);
+        System.out.println("hello");
     }
-//    @Autowired
-
-
 }
